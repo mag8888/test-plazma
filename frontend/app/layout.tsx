0fa43716ca792c540de2635559e250298f81f722
MAG <208105026+mag8888@users.noreply.github.com>
2025-12-19 07:26:41 +0300

    style: remove global bottom padding to fix game ui layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pb-20`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TelegramProvider>
           {children}
